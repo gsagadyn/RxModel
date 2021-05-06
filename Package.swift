312cc9163f55286704f69f9d69097e42c8b242b4
Kamil Modzelewski <kammodze@gmail.com>
2021-05-07 01:20:20 +0200

    Create Package.swift

---
+++ Package.swift
@@ -0,0 +1,16 @@
+// swift-tools-version:5.3
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+import PackageDescription
+
+let package = Package(
+    name: "RxModel",
+    platforms: [
+        .iOS(.v11)
+    ],
+    products: [
+        .library(name: "RxModel", targets: ["RxModel"])
+    ],
+    targets: [
+        .target(name: "RxModel", dependencies: [], path: "RxModel", resources: [])
+    ]
+)
